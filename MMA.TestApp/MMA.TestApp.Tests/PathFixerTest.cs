3494d301a26b9902243393da7c37b81278ca3108
Malik <Mateusz.Malik@hidglobal.com>
2019-11-23 00:40:42 +0100

    tests

---
+++ MMA.TestApp/MMA.TestApp.Tests/PathFixerTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMA.TestApp.Tests
+{
+    [TestFixture]
+    public class PathFixerTest
+    {
+        [Test]
+        [Ignore("No reason")]
+        public void ShouldCorrectPath()
+        {
+            var cut = new PathFixer();
+            var entries = cut.GetPathEntries();
+            var distinctEntries = entries.Distinct();
+            cut.SetPath(distinctEntries.ToArray());
+        }
+    }
+}
---
+++ MMA.TestApp/MMA.TestApp/PathFixer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMA.TestApp
+{
+    public class PathFixer
+    {
+        public string[] GetPathEntries()
+        {
+            return Environment.GetEnvironmentVariable("PATH", EnvironmentVariableTarget.Machine).Split(new string[] { ";" }, StringSplitOptions.RemoveEmptyEntries) ;
+        }
+
+        public void SetPath(string[] entries)
+        {
+            var pathEntry = string.Join(";", entries);
+            Environment.SetEnvironmentVariable("PATH", pathEntry, EnvironmentVariableTarget.Machine);
+        }
+    }
+}
